eb85e63654776f1de4b8dc31f557f83584caf0ea
Jenkins <jenkins@srv.apres.online>
2017-05-31 22:25:02 +0300

    2017-05-31:22:25- mainchanges -Commit

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-
-read -p "Enter email for notifications:" mail
+if [ $1 = "uninstall" ]; then
+    rm -rf /usr/bin/abf
+    exit 0
+fi
+Select="0"
 wdir=/usr/bin/abf
 mkdir -p $wdir && cp -v lib/check.badlogin.sh $wdir/abf.sh
     if [ -f /etc/redhat-release ]; then
@@ -11,7 +14,18 @@ mkdir -p $wdir && cp -v lib/check.badlogin.sh $wdir/abf.sh
         sed -i 's/wlog=""/wlog=\/var\/log\/auth.log/' $wdir/abf.sh
     fi
 sed -i 's|bcan=""|bcal='$wdir'|' $wdir/abf.sh
-sed -i 's/mail=""/mail='$mail'/' $wdir/abf.sh
-chmod +x $wdir/abf.sh && $wdir/abf.sh
 
-echo "0"
+while [ $Select -lt 1 ]; do
+    read -p "Do you want to get email notifications?[Yy/Nn]" noty
+    case $noty in
+        [Yy]* ) Select="2";;
+        [Nn]* ) Select="3";;
+    esac
+   done
+    if [ $Select = "2" ]; then
+        read -p "Enter email for notifications:" mail
+        sed -i 's/mail=""/mail='$mail'/' $wdir/abf.sh
+    fi
+chmod +x $wdir/abf.sh && $wdir/abf.sh
+echo "Done."
+
